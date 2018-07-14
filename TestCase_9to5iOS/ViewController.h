ad0f20bcc3660042603f4c988546d46f60c36c00
Deepak singh rawat <deepak.rawat@spicedigital.in>
2018-07-14 11:58:22 +0530

    Initial Commit

---
+++ TestCase_9to5iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestCase_9to5iOS
+//
+//  Created by admin on 14/07/18.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestCase_9to5iOSTests/TestCase_9to5iOSTests.m
@@ -0,0 +1,39 @@
+//
+//  TestCase_9to5iOSTests.m
+//  TestCase_9to5iOSTests
+//
+//  Created by admin on 14/07/18.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestCase_9to5iOSTests : XCTestCase
+
+@end
+
+@implementation TestCase_9to5iOSTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
